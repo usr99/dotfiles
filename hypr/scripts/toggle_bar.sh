296144cbd56415d271ecefe35a7bfa7078061dc9
mamartin <mamartin@mail>
2024-03-02 15:10:55 +0100

    eww var MONITOR_MODE

--- hypr/scripts/toggle_bar.sh
+++ hypr/scripts/toggle_bar.sh
@@ -2,6 +2,9 @@
 
 if [[ `eww active-windows | grep bar` == "" ]]; then
 	eww open bar-monitor0
+	if [[ `eww get MONITOR_MODE` == "double" ]]; then
+		eww open bar-monitor1
+	fi
 else
 	eww close-all 
 fi
