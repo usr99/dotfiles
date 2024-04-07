3c80e22351299ebdfe25915b20b4ea77922b9ab4
mamartin <mamartin2@proton.me>
2024-04-08 01:19:48 +0200

    rework setup.sh with comments and more

--- setup.sh
+++ setup.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
 ZSHRC="$(cat << EOF
-source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+# Some aliases that I like
 alias grep=rg
 alias vim=nvim
 alias open=xdg-open
+
+# Zoxide configuration
+eval "$(zoxide init zsh)"
 alias cd=z
+
+# Add cargo install dir to PATH
 export PATH=~/.cargo/bin:$PATH
 
-hypr() {
-    cd ~
+# Make nvim the default editor for git and others
+export EDITOR=nvim
 
-    exec /usr/bin/pipewire & # Audio server
-    exec /usr/bin/wireplumber & # Session manager
-    exec /usr/bin/pipewire-pulse & # PulseAudio interface
-
-    exec Hyprland
-}
-
-switch_monitor_mode() {
-	if [[ `eww get MONITOR_MODE` == "single" ]]; then
-		eww update MONITOR_MODE=double
-		ln -sf ~/.config/hypr/double_monitor.conf ~/.config/hypr/monitors.conf
-	else
-		eww update MONITOR_MODE=single
-		ln -sf ~/.config/hypr/single_monitor.conf ~/.config/hypr/monitors.conf
-	fi
-}
 EOF
 )"
 
@@ -43,6 +32,7 @@ sudo pacman -S \
 	ttf-jetbrains-mono-nerd ttf-firacode-nerd \
 	zoxide fzf
 
+# Install yay to help with AUR installations 
 git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 yay -Y --gendb && yay -Syu --devel && yay -Syu --devel --save
 rm -rf ./yay
@@ -57,20 +47,19 @@ yay -S \
 	libva-nvidia-driver-git \
 	zsh-theme-powerlevel10k-git
 
-enable_service(name) {
-	sudo systemctl start name
-	sudo systemctl enable name
-}
+sudo systemctl enable --now iwd # Wireless daemon
+sudo systemctl enable --now dhcpcd
+sudo systemctl enable --now bluetooth
 
-enable_service(iwd)
-enable_service(dhcpcd)
-enable_service(bluetooth)
+# Default firewall settings
 sudo ufw enable
 sudo ufw default deny
 
+# Configure SSH 
 cd ~/.ssh
-ssh-keygen -t rsa
-touch authorized_keys
+ssh-keygen -t rsa # RSA keys generation
+touch authorized_keys # File where are stored public keys to enable key-based authentication 
+# Add configuration for my other computers 
 echo "Host $SSH_HOSTNAME" > config
 echo "    HostName $SSH_IP" >> config
 echo "    User $SSH_USER" >> config
@@ -78,22 +67,31 @@ echo "    RequestTTY yes" >> config
 echo "    RemoteCommand /usr/bin/zsh -l -c 'tmux'" >> config
 cd - 
 
+# Manually install the Icomoon Feather font
+# it's only used in some rofi menus 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts
 wget https://github.com/adi1090x/rofi/raw/master/fonts/Icomoon-Feather.ttf
 cd -
 
+# Configure git
+# todo: add more options
 git config --global user.mail $GIT_MAIL
 git config --global user.name $GIT_USERNAME
 
+# Install the TMUX plugin manager
 mkdir -p ~/.tmux/plugins
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Install OhMyZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo -e "$ZSHRC" >> ~/.zshrc
 
+# Create user directories
+# Downloads, Pictures, etc.
 LC_ALL=C xdg-user-dirs-update
 
+# Rust toolchain
 rustup default stable
 rustup component add rust-analyzer
 
