84d84b2f652e7686d5346536269f82e4461917c1
mamartin <mamartin2@proton.me>
2024-04-26 18:54:23 +0200

    insane install script

---
+++ install.sh
@@ -0,0 +1,401 @@
+#!/bin/bash
+
+SSH_DIR="$HOME/.ssh"
+ZSHRC="$HOME/.zshrc"
+TMPFILE=".tmp.wiguewihfg7sdrtg6d7s8tgw8ej"
+
+RED='\033[0;31m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+PURPLE='\033[0;35m'
+YELLOW='\033[0;33m'
+WHITE='\033[0;0m'
+
+function printc {
+	str=$1
+	color=$2
+
+	echo -en $color
+	echo -n  $str
+	echo -e  $WHITE
+}
+
+function wait_for_input {
+	printc "Press [ENTER] to continue" $YELLOW
+	read -n1
+}
+
+function print_header {
+	text=$1
+	case $2 in 
+		1)
+			color=$BLUE
+		;;
+		2)
+			color=$PURPLE
+		;;
+		3)
+			color=$YELLOW
+		;;
+		*)
+			color=$BLUE
+		;;
+	esac
+	
+	printc "$text" $color
+}
+
+function pacman {
+	print_header "Install packages from Arch repositories"
+	wait_for_input
+
+	sudo pacman -Syu \
+		nftables ufw \
+		bqinutils make gcc pkg-config fakeroot flex bison \
+		hyprland gtk3 gtk-layer-shell qt5-wayland qt5ct libva \
+		pipewire pipewire-pulse pipewire-audio pipewire-jack wireplumber \
+		noise-suppression-for-voice \
+		bluez bluez-utils \
+		thunar alacritty firefox discord pulsemixer \
+		xdg-utils xdg-user-dirs wl-clipboard grim slurp feh \
+		zsh tmux neovim unzip ripgrep fd \
+		rustup \
+		ttf-jetbrains-mono-nerd ttf-firacode-nerd \
+		zoxide fzf
+}
+
+function paru {
+	print_header "Install paru and packages from AUR"
+	wait_for_input
+
+	git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
+	cd - && rm -r paru 
+
+	paru -S \
+		rofi-lbonn-wayland-git \
+		hyprshade hyprpicker-git \
+		swaylock-effects-git \
+		swww \
+		eww-git \
+		libva-nvidia-driver-git \
+		zsh-theme-powerlevel10k-git
+}
+
+function networking {
+	print_header "Setup networking"
+	wait_for_input
+
+	print_header "Enable networking services" 2
+	sudo systemctl enable --now iwd # Wireless daemon
+	sudo systemctl enable --now dhcpcd
+	sudo systemctl enable --now NetworkManager 
+	sudo systemctl enable --now bluetooth
+
+	print_header "Enable firewall" 2
+	sudo ufw enable
+	sudo ufw default deny
+
+	print_header "Configure SSH" 2
+	print_header "Generate RSA key pair" 3
+	ssh-keygen -t rsa
+	print_header "Create SSH authorized_keys file" 3
+	touch $SSH_DIR/authorized_keys
+	sshconfig
+}
+
+function sshconfig {
+	echo -en $YELLOW
+	read -n 1 -p "Would you like to add a SSH host ? [y/N] " input
+	echo -en $WHITE
+	echo
+
+	echo $SSH_DIR/config
+
+	if [[ "$input" != "y" ]]; then
+		return 
+	fi
+
+	read -p "Host ? " host
+	read -p "IP ? " addr
+	read -p "Username ? " user
+
+	echo "Host $host" > $SSH_DIR/config
+	echo "    HostName $addr" >> $SSH_DIR/config
+	echo "    User $user" >> $SSH_DIR/config
+	echo "    RequestTTY yes" >> $SSH_DIR/config
+	echo "    RemoteCommand /usr/bin/zsh -l -c 'tmux'" >> $SSH_DIR/config
+}
+
+function terminal {
+	print_header "Configure terminal environment"
+	wait_for_input
+
+	print_header "Install Tmux Plugin Manager (TPM)" 2
+	mkdir -p ~/.tmux/plugins
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+	print_header "Install OhMyZSH" 2
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+	print_header "Import .zshrc settings" 2
+	cat << EOF >> $ZSHRC
+# Enable powerlevel10k theme
+source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
+
+# Enable zoxide 
+eval \"\$(zoxide init zsh)\"
+
+# Add cargo install dir to PATH
+export PATH=~/.cargo/bin:\$PATH
+
+# Make nvim the default editor for git and others
+export EDITOR=nvim
+
+# Some aliases that I find useful 
+alias grep=rg
+alias vim=nvim
+alias open=xdg-open
+alias cd=z
+EOF
+
+	print_header "Enable plugins : web-search copyfile  " 2
+	sed 's/plugins=(git/& web-search copyfile/' -i $ZSHRC
+}
+
+function misc {
+	print_header "Miscellaneous ..."
+	wait_for_input
+
+	print_header "Manual installation of fonts" 2
+	mkdir -p ~/.local/share/fonts
+	cp fonts/* ~/.local/share/fonts
+
+	print_header "Setup home directory" 2
+	LC_ALL=C xdg-user-dirs-update
+}
+
+function dev {
+	print_header "Dev environment setup"
+	wait_for_input
+
+	print_header "GIT configuration" 2
+	read -p "Email Address ? " mail
+	read -p "Username ? " username 
+	git config --global user.email $mail
+	git config --global user.name $username
+
+	print_header "Rust toolchain" 2
+	rustup default stable
+	rustup component add rust-analyzer
+}
+#
+# function install_desktop {
+# 	# pacman
+# 	# paru
+# 	# networking
+# 	# terminal
+# 	# dev
+# 	# misc
+# }
+#
+# function create_user {
+#
+# }
+#
+# function locale {
+#
+# }
+#
+# function grub {
+#
+# }
+#
+# function arch-chroot {
+# 	# locale
+# 	# grub
+# 	# create_user
+# }
+#
+# function pacstrap {
+#
+# }
+#
+function diy_partitioning {
+	local -n _efi=$1
+	local -n _swap=$2
+	local -n _home=$3
+	local -n _linux=$4
+
+	echo -en $GREEN"EFI$WHITE partition ? "
+	read _efi 
+	echo -en $GREEN"SWAP$WHITE partition ? "
+	read _swap 
+	echo -en $GREEN"HOME$WHITE partition ? "
+	read _home 
+	echo -en $GREEN"LINUX$WHITE partition ? "
+	read _linux 
+}
+
+function read_with_default {
+	text=$1
+	local -n value=$2
+	defvalue=$3
+	pattern=$4
+
+	while true; do
+		echo -en $text "? "
+		read input
+
+		if [[ "$input" != "" ]]; then
+			if [[ "$input" =~ $pattern ]]; then
+				value=$input
+				return 
+			fi
+			echo -e $RED"Your input does not match this pattern: $pattern"$WHITE
+		else
+			value=$defvalue
+			return
+		fi
+	done
+
+}
+
+function create_partition {
+	type=$1
+	size=$2
+
+	echo n >> $TMPFILE
+	echo   >> $TMPFILE
+	echo   >> $TMPFILE
+
+	if [[ "$size" != "" ]]; then
+		echo -n "+" >> $TMPFILE
+	fi
+	echo $size >> $TMPFILE
+
+	if [[ "$type" != "" ]]; then
+		echo t >> $TMPFILE
+		echo type >> $TMPFILE
+	fi
+}
+
+function default_partitioning {
+	local -n _efi=$1
+	local -n _swap=$2
+	local -n _home=$3
+	local -n _linux=$4
+
+	# read -p "Target disk ? " disk
+	read_with_default "Type of partition table [MBR/gpt]" tabletype "MBR" '^(mbr|MBR|gpt|GPT)$'
+	read_with_default "Size of $GREEN""EFI$WHITE partition [512M]" sizeof_efi "512M" '^[0-9]*(K|M|G|T|P)$'
+	read_with_default "Size of $GREEN""SWAP$WHITE partition [4G]" sizeof_swap "4G" '^[0-9]*(K|M|G|T|P)$'
+	read_with_default "Size of $GREEN""HOME$WHITE partition [30G]" sizeof_home "30G" '^[0-9]*(K|M|G|T|P)$'
+
+	echo $tabletype
+	if [[ "${tabletype^^}" = "MBR" ]]; then
+		tabletype="o"
+	else
+		tabletype="g"
+	fi
+
+	echo $tabletype > $TMPFILE # init partition
+	create_partition "uefi" $sizeof_efi
+	create_partition "swap" $sizeof_swap
+	create_partition "home" $sizeof_home
+	create_partition ""     $sizeof_home
+	echo w >> $TMPFILE # save to disk
+	cat $TMPFILE | fdisk $disk
+	rm $TMPFILE
+
+	_efi="$disk""n1"
+	_swap="$disk""n2"
+	_home="$disk""n3"
+	_linux="$disk""n4"
+}
+
+function partitioning {
+	# diy
+	
+		default_partitioning efi swap home linux 
+
+	print_header "Partitioning"
+	wait_for_input
+
+	read -p "Do you want this script to format your disk ? [y/N] " yesno
+
+	if [[ "$yesno" != "y" ]]; then
+		diy_partitioning efi swap home linux 
+	else
+		default_partitioning efi swap home linux 
+	fi
+
+	echo $efi $swap $home $linux
+
+	# diy ?
+	#	yes
+	#		go back if you didnt do it yet
+	#		no need to format
+	#		uefi ?
+	#		swap ?
+	#		home ?
+	#		linux ?
+	#	no
+	#		uefi size ? (512M)
+	#		swap size ? (4G)
+	#		home size ? (30G)
+	#		gpt or mbr ?
+
+	# sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk $disk
+ #      o # clear the in memory partition table
+ #      n # create efi partition
+	#
+	#
+ #      +512M
+ #      t
+ #      uefi
+ #      n # create swap partition
+	#
+	#
+ #      +4G
+ #      t
+ #      swap
+ #      n # create home partition
+	#
+	#
+ #      +30G
+ #      t
+ #      home 
+ #      n # create fs partition
+	#
+	#
+	#
+ #      p # print the in-memory partition table
+ #      w # write to disk before exit
+# EOF
+}
+
+function install_system {
+	# check efi boot
+	partitioning
+	# pacstrap
+	# arch-chroot
+	
+	# umount -R /mnt
+	# warn for grub
+	# warn for dns issues 
+	# run install desktop next time :)
+	# reboot
+}
+
+case $1 in
+	"system")
+		install_system
+	;;
+	"desktop")
+		install_desktop
+	;;
+	*)
+		echo "Specify one of {system, desktop}"
+	;;
+esac
+
